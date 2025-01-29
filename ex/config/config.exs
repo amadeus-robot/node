ae8d45d865d9c0d3dae82e704c0e94d176cd78e0
root <polly2896@gmail.com>
2025-01-29 20:22:29 +0000

    rpc Server Implementation

--- ex/config/config.exs
+++ ex/config/config.exs
@@ -8,3 +8,6 @@ config :ama, :entry_size, 1432
 config :ama, :tx_size, 768
 config :ama, :attestation_size, 512
 config :ama, :quorum, 3
+
+config :ama, :rpc_port, 4000
+
--- ex/config/runtime.exs
+++ ex/config/runtime.exs
@@ -12,6 +12,9 @@ IO.puts "config folder is #{work_folder}"
 #Bind Interaces
 config :ama, :http_ip4, ((System.get_env("HTTP_IP4") || "0.0.0.0") |> :unicode.characters_to_list() |> :inet.parse_ipv4_address() |> (case do {:ok, addr}-> addr end))
 config :ama, :http_port, (System.get_env("HTTP_PORT") || "1090") |> :erlang.binary_to_integer()
+config :ama, :rpc_port, (System.get_env("HTTP_PORT") || "4000") |> :erlang.binary_to_integer()
+
+
 
 config :ama, :udp_ipv4_tuple, ((System.get_env("UDP_IPV4") || "0.0.0.0") |> :unicode.characters_to_list() |> :inet.parse_ipv4_address() |> (case do {:ok, addr}-> addr end))
 config :ama, :udp_port, 36969
